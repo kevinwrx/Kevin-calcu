cf0c0dd1a663f4de1fc4a74d0422e0a4ef88a7e6
KarmaSora <karammtr@gmail.com>
2022-05-20 23:13:30 +0200

    fungerar och kommenterad. Kan fixa lite mergit add .

--- js/script.js
+++ js/script.js
@@ -7,96 +7,86 @@ let lcd = null; // displayen
 let memory = 0; // Lagrat/gamlat värdet från display
 let arithmetic = null; // Vilken beräkning som skall göras +,-, x eller /
 function init() {
-    lcd = document.getElementById('lcd');
-    let keyBoard = document.getElementById('keyBoard')
-    keyBoard.onclick = buttonClick;
-    let clear = document.getElementById('clear'); // skapa en variable som får värdet av id clear
-    clear.onclick = memClear; // kör funktionen clearLCD och tömmer lcd när man clicker på clear knappen
-    let commaT = document.getElementById('comma');
-    commaT.onclick = addComma;
+  lcd = document.getElementById('lcd');
+  let keyBoard = document.getElementById('keyBoard')
+  keyBoard.onclick = buttonClick;
+  let clear = document.getElementById('clear'); // skapa en variable som får värdet av id clear
+  clear.onclick = memClear; // kör funktionen clearLCD och tömmer lcd när man clicker på clear knappen
+  let commaT = document.getElementById('comma'); // skapar variabeln commaT och ger den värdet av comma
+  commaT.onclick = addComma; // när man klicker på knappen med id comma som nu har samma värde som commaT då skall funktionen addComma kallas
 
-    let enter = document.getElementById('enter'); //deklerera variabeln enter
-    enter.onclick = calculate; // kopplar enter till funktionen calculate som ska köras vid onclick
+  let enter = document.getElementById('enter'); //deklerera variabeln enter
+  enter.onclick = calculate; // kopplar enter till funktionen calculate som ska köras vid onclick
 
-    const result = 0;
+  const result = 0; // skapar en varaibel med med värdet 0, const är ett sätt att deklarera variabler och liknar let
 
 }
 /**
  * Händelsehanterare för kalkylatorns tangentbord
  */
 function buttonClick(e) {
-    let btn = e.target.id; //id för den tangent som tryckte ner
-    // kollar om siffertangent är nedtryckt
-    if (btn.substring(0, 1) === 'b') {
-        let digit = btn.substring(1, 2); // plockar ut siffran från id:et
-        lcd.value += digit; // skriver ut digit/ tal  genom att plocka talen från knabbar vas id har bokstaven b i form av en string
-    } else { // Inte en siffertangent, övriga tangenter.
-      if(btn.substring(0, 1) === 'a'){
-        setOperator('+');
-      }
-      if(btn.substring(0, 1) === 's'){
-        setOperator('-');
-      }
-      if (btn.substring(0, 1) === 'm') {
-        setOperator('*');
-      }
-      if (btn.substring(0, 1) === 'd') {
-        setOperator('/');
-      }
+  let btn = e.target.id; //id för den tangent som tryckte ner
+  // kollar om siffertangent är nedtryckt
+  if (btn.substring(0, 1) === 'b') {
+    let digit = btn.substring(1, 2); // plockar ut siffran från id:et
+    lcd.value += digit; // skriver ut digit/ tal  genom att plocka talen från knabbar vas id har bokstaven b i form av en string
+  } else { // Inte en siffertangent, övriga tangenter.
+    if (btn.substring(0, 1) === 'a') { // kontrolerar om idet på knappen som trycks börjar med en viss bokstav, och sedan kör en funktion
+      setOperator('+'); // kör funktionen setOperator('+')
     }
+    if (btn.substring(0, 1) === 's') {
+      setOperator('-');
+    }
+    if (btn.substring(0, 1) === 'm') {
+      setOperator('*');
+    }
+    if (btn.substring(0, 1) === 'd') {
+      setOperator('/');
+    }
+  }
 }
 /**
  *  Lägger till siffra på display.
  */
-function addDigit(digit) {
-  lcd.value += digit;
+function addDigit(digit) { // digits dekleration finns längre upp
+  lcd.value += digit; // adderar tal till lcd displayen, digit är siffran som finns efter bokstaven b   i idet på det valda knappen
 }
 /**
  * Lägger till decimaltecken
  */
-function addComma() {
-  lcd.value += "." ;
+function addComma() { // en funktion för att tilläga  ett kommatecken till lcd displayen
+  lcd.value += ".";
 }
 /**
  * Sparar operator.
  * +, -, *, /
  */
 
+function setOperator(operator) {
+  switch (operator) { // switch case , ifall .... då ...
+    case '+': // ifall man klicker på + då ...
+      memory = lcd.value; // memory får värdet av displayen
+      clearLCD(); // displayen clearas
+      arithmetic = '+'; //arithmetic får värdet av '+'
+      break;
 
+    case '-':
+      memory = lcd.value;
+      clearLCD();
+      arithmetic = '-';
+      break;
 
-function setOperator(operator){
-switch (operator) {
-  case '+':
-  memory = lcd.value;
+    case '*':
+      memory = lcd.value;
+      clearLCD();
+      arithmetic = '*'
+      break;
 
-  clearLCD();
-
-  //result = Number(memory) + Number(lcd.value);
-  //lcd.value = result;
-  arithmetic = '+';
-  break;
-
-
-  case '-':
-  memory = lcd.value;
-
-clearLCD();
-
-arithmetic = '-';
-    break;
-
-  case '*':
-  memory = lcd.value;
-  clearLCD();
-  arithmetic = '*'
-
-    break;
-
-  case '/':
-memory = lcd.value;
-clearLCD();
-arithmetic = '/';
-    break;
+    case '/':
+      memory = lcd.value;
+      clearLCD();
+      arithmetic = '/';
+      break;
   }
 }
 /**
@@ -104,31 +94,29 @@ arithmetic = '/';
  */
 function calculate() {
 
-switch (arithmetic) {
-  case '+':
-  result = Number(lcd.value) + Number(memory);
-  lcd.value = result;
-    break;
+  switch (arithmetic) { // swich case för arthimatic.
+    case '+': // om arthimatic = '+',   (se längre upp  där det uppreps)
+      result = Number(lcd.value) + Number(memory); // varaibelen resultat på värdet av lcd och det gamla värdet som finns i memory. Number() är för att kunna räkna med decimaltal, hade kunnat använt paresInt(), eller paresfloat()
+      lcd.value = result; // resultatet sätts i lcd displayen.
+      break;
 
-  case '-':
-  result =  Number(memory) - Number(lcd.value) ;
-  lcd.value = result;
-    break;
+    case '-':
+      result = Number(memory) - Number(lcd.value);
+      lcd.value = result;
+      break;
 
-  case '*':
-  result = Number(lcd.value) * Number(memory);
-  lcd.value = result;
-    break;
+    case '*':
+      result = Number(lcd.value) * Number(memory);
+      lcd.value = result;
+      break;
 
-  case '/':
-  result =  Number(memory) / Number(lcd.value) ;
-  lcd.value =  result;
+    case '/':
+      result = Number(memory) / Number(lcd.value);
+      lcd.value = result;
+      break;
+    default:
 
-    break;
-
-  default:
-
-}
+  }
 
 }
 
@@ -137,13 +125,13 @@ switch (arithmetic) {
 
 /** Rensar display */
 function clearLCD() {
-    lcd.value = '';
-    isComma = false;
+  lcd.value = '';
+  isComma = false;
 }
 /** Rensar allt, reset */
-function memClear(){
-    memory = 0;
-    arithmetic = null;
-    clearLCD();
+function memClear() {
+  memory = 0;
+  arithmetic = null;
+  clearLCD();
 }
 window.onload = init;
